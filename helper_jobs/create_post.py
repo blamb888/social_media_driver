80ffe521a795416eda3a08d792509e4f1b4c659f
Brandon Lamb <blamb888@gmail.com>
2023-10-31 20:58:42 +0900

    minor updates TODO rework adding picture for posts

--- helper_jobs/create_post.py
+++ helper_jobs/create_post.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from select_image import select_image_from_drive
+from helper_jobs.select_image_from_drive import select_image_from_drive
 import time
 
 def create_post(browser, title, etsy_url):
@@ -9,7 +9,7 @@ def create_post(browser, title, etsy_url):
 
     # Handle popup
     try:
-        popup_close_button = WebDriverWait(browser, 20).until(
+        popup_close_button = WebDriverWait(browser, 30).until(
             EC.element_to_be_clickable((By.XPATH, "//button[@id='pendo-close-guide-0f60d048']"))  
         )
         popup_close_button.click()
@@ -18,25 +18,25 @@ def create_post(browser, title, etsy_url):
         print("No popup appeared.")
     
     # Click on the 'Create Post' button
-    create_post_button = WebDriverWait(browser, 20).until(
+    create_post_button = WebDriverWait(browser, 30).until(
         EC.element_to_be_clickable((By.XPATH, "//button[.//div[contains(text(), 'Create Post')]]"))
     )
     create_post_button.click()
     
     # Click on the 'Use the AI Assistant' button
-    ai_assistant_button = WebDriverWait(browser, 20).until(
+    ai_assistant_button = WebDriverWait(browser, 30).until(
         EC.element_to_be_clickable((By.XPATH, "//button[@id='ai-assistant-placeholder-button']"))
     )
     ai_assistant_button.click()
     
     # Enter the product title into the AI Assistant's text box
-    ai_assistant_textbox = WebDriverWait(browser, 20).until(
+    ai_assistant_textbox = WebDriverWait(browser, 30).until(
         EC.element_to_be_clickable((By.XPATH, "//textarea[@id='prompt']"))
     )
     ai_assistant_textbox.send_keys(title)
     
     # Click on the 'Generate' button
-    generate_button = WebDriverWait(browser, 20).until(
+    generate_button = WebDriverWait(browser, 30).until(
         EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']"))
     )
     generate_button.click()
@@ -51,7 +51,7 @@ def create_post(browser, title, etsy_url):
     
     # Handle new popup
     try:
-        new_popup_close_button = WebDriverWait(browser, 20).until(
+        new_popup_close_button = WebDriverWait(browser, 30).until(
             EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Close']"))  
         )
         new_popup_close_button.click()
@@ -60,14 +60,14 @@ def create_post(browser, title, etsy_url):
         print("No new popup appeared.")
     
     # Click on the 'Insert' button
-    insert_button = WebDriverWait(browser, 20).until(
+    insert_button = WebDriverWait(browser, 30).until(
         EC.element_to_be_clickable((By.XPATH, "//button[text()='Insert']"))
     )
     insert_button.click()
         
     # Handle new popup
     try:
-        new_popup_close_button = WebDriverWait(browser, 20).until(
+        new_popup_close_button = WebDriverWait(browser, 30).until(
             EC.element_to_be_clickable((By.XPATH, "//button[@id='pendo-close-guide-33adcb58']"))  
         )
         new_popup_close_button.click()
@@ -79,19 +79,19 @@ def create_post(browser, title, etsy_url):
     close_button.click()
     print("AI Assistant closed.")
 
-    post_textbox = WebDriverWait(browser, 20).until(
+    post_textbox = WebDriverWait(browser, 30).until(
         EC.element_to_be_clickable((By.XPATH, "//div[@data-testid='composer-text-area']"))
     )
     post_textbox.send_keys(f'\n\n{etsy_url}\n')  # append the product URL on a new line
 
     # Check for Suggested media carousel
     try:
-        suggested_media_header = WebDriverWait(browser, 20).until(
+        suggested_media_header = WebDriverWait(browser, 30).until(
             EC.presence_of_element_located((By.XPATH, "//div[text()='Suggested media (8):']"))
         )
         if suggested_media_header:
             print("Suggested media found. Selecting first option.")
-        first_option_button = WebDriverWait(browser, 20).until(
+        first_option_button = WebDriverWait(browser, 30).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@class='_suggestionsScrollContainer_13oy6_121']/button[1]"))
         )
         first_option_button.click()
@@ -102,19 +102,19 @@ def create_post(browser, title, etsy_url):
         select_image_from_drive(browser, etsy_url)
         
     # # Click on the 'Customize for each network' button
-    # customize_button = WebDriverWait(browser, 20).until(
+    # customize_button = WebDriverWait(browser, 30).until(
     #     EC.element_to_be_clickable((By.XPATH, "//button[text()='Customize for each network']"))
     # )
     # customize_button.click()
     # print("Clicking customize for each network button...")
         
     # Check if 'Add to Queue' button is disabled
-    add_to_queue_button = WebDriverWait(browser, 20).until(
+    add_to_queue_button = WebDriverWait(browser, 30).until(
         EC.presence_of_element_located((By.XPATH, "//button/div[text()='Add to Queue']"))
     )
     add_to_queue_button.click()
     
-    # button_disabled = WebDriverWait(browser, 20).until(
+    # button_disabled = WebDriverWait(browser, 30).until(
     #     EC.presence_of_element_located((By.XPATH, "//div[@data-testid='stacked-save-buttons-section'][@data-disabled='true']"))
     # )
 
@@ -130,7 +130,7 @@ def create_post(browser, title, etsy_url):
     #         div.click()
     #         print("Expanding div...")
     #         # Wait for the suggestions to load
-    #         WebDriverWait(browser, 20).until(
+    #         WebDriverWait(browser, 30).until(
     #             EC.presence_of_element_located((By.XPATH, "//button[contains(@class, '_thumbnailContainer')]"))
     #         )
     #         # Click the first suggested image
