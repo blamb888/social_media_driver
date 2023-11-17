a243e3a33f0784bff3a54bc8c6dfed15cd679c0e
Brandon Lamb <blamb888@gmail.com>
2023-11-17 14:37:51 +0900

    make browser fullscreen with options

--- helper_jobs/initialize_browser.py
+++ helper_jobs/initialize_browser.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import os
 from dotenv import load_dotenv
 
 def initialize_browser():
     # Configure the headless browser
-    # options = Options()
+    options = Options()
     # options.add_argument("--headless")
     # options.add_argument("--disable-gpu")
+    options.add_argument("--start-maximized")
     # options.add_argument("--remote-debugging-port=9222")  # specify a port
 
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=options)
     return browser
