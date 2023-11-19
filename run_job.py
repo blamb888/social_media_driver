3161e1e1f1c8844da348fd447097e3a80d5465cf
Brandon Lamb <blamb888@gmail.com>
2023-11-19 16:18:34 +0900

    clean up some things get ready to set up cron job and post creation loop

--- run_job.py
+++ run_job.py
@@ -3,42 +3,59 @@ from helper_jobs.initialize_browser import initialize_browser
 from helper_jobs.login import login
 from helper_jobs.logout import logout
 from helper_jobs.create_post import create_post
-from helper_jobs.disable_pinterest_channel import disable_pinterest_channel
+from selenium.webdriver.common.by import By
 import traceback
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+MAX_POSTS = 10
+
+
+def get_scheduled_post_count(browser):
+    browser.get('https://publish.buffer.com/calendar/week')
+    try:
+        wait = WebDriverWait(browser, 10)  # wait for a maximum of 10 seconds
+        
+        # Modify the index in the XPath as needed (2 for the second occurrence, 3 for the third, etc.)
+        post_count_element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='root']/div/div[1]/div[1]/div[1]/div/ul[2]/div[1]/div/li/span")))
+        post_count = int(post_count_element.text) if post_count_element.text else 0
+        return post_count
+    except Exception as e:
+        print(f"Error while trying to get the post count: {e}")
+        return None
 
 browser = initialize_browser()
 
 try:
-    product_title, etsy_url = get_product_title_local()
-    print(f'Product Title: {product_title}')
-
     login(browser)
 
-    # Check if the current URL is the desired one
     if browser.current_url != 'https://publish.buffer.com/calendar/week':
-        # If not, navigate to the desired URL
         browser.get('https://publish.buffer.com/calendar/week')
 
-    # Check again if you are on the correct page
     if browser.current_url == 'https://publish.buffer.com/calendar/week':
         print("Logged into Buffer and navigated to the calendar successfully!")
     else:
         print("Failed to navigate to the calendar page.")
+        raise Exception("Navigation to Buffer calendar page failed")
+
+    current_post_count = get_scheduled_post_count(browser)
+    if current_post_count is None:
+        raise Exception("Could not retrieve post count.")
+
+    if current_post_count < MAX_POSTS:
+        product_title, etsy_url = get_product_title_local()
+        print(f'Product Title: {product_title}')
         
-    # disable_pinterest_channel(browser)
-    # print("Pinterest channel disabled.")
-    
-    # Add a wait time
-    # time.sleep(25)  # waits for 25 seconds
-    
-    create_post(browser, product_title, etsy_url)
-    print("Post created successfully!")
+        # Create the post
+        create_post(browser, product_title, etsy_url)
+        print("Post created successfully!")
+    else:
+        print("Maximum number of posts already scheduled.")
 
 except Exception as e:
     print(f"An error occurred: {e}")
-    print(traceback.format_exc())  # This will print the stack trace
+    print(traceback.format_exc())
 
 finally:
-    # Close the browser
     browser.quit()
