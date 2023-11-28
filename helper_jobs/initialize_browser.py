2aec792254c266d9deb03f28e119d3ebf6297ef1
Brandon Lamb <blamb888@gmail.com>
2023-11-29 06:30:07 +0900

    begin switch to headless mode. it works sometimes but there are errors to debug

--- helper_jobs/initialize_browser.py
+++ helper_jobs/initialize_browser.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 def initialize_browser():
     # Configure the headless browser
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     # options.add_argument("--disable-gpu")
-    options.add_argument("--start-maximized")
+    options.add_argument("--window-size=1920,1080")  # Set the desired window size
     # options.add_argument("--remote-debugging-port=9222")  # specify a port
 
     browser = webdriver.Chrome(options=options)
